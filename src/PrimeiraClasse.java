33e85e41f25b0d3930f46f8aae48f4a74e48f22c
felipoxy1 <luizfelipepassos2015@gmail.com>
2025-06-07 21:58:13 -0300

    first commit

---
+++ src/PrimeiraClasse.java
@@ -0,0 +1,7 @@
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        String AloProfessor = "Eai Professor bao? ";
+        System.out.println(AloProfessor + "Meu nome e Luiz e estou comecando as aulas de back-end");
+
+    }
+}
